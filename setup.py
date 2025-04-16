57efae69a2d7d03b3b7fe4c15592239ed16e6ecb
KordyakIM <30699602+KordyakIM@users.noreply.github.com>
2025-04-16 19:02:32 +0500

    Add files via upload

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="orange3-autogluon-timeseries",
-    version="1.0.2",
+    version="1.0.3",
     description="AutoGluon Time Series forecasting widget for Orange3",
     long_description=long_description,
     long_description_content_type="text/markdown",  # <--- это важно
